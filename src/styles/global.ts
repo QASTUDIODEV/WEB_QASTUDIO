7dd5cb4a439d813c1d47f0ecd7237417f128ea01
hijjoy <hijjoy@naver.com>
2025-01-01 19:58:17 +0900

    feat: style theme 설정 #1 - font, color, shadow, align 테마 설정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+    ${reset}
     @font-face {
         font-family: 'Pretendard';
         src: url('/fonts/PretendardVariable.woff2') format('woff2');
@@ -10,8 +12,9 @@ const GlobalStyle = createGlobalStyle`
 
     * {
         font-family: 'Pretendard', sans-serif;
-        margin: 0;
         box-sizing: border-box;
+        margin: 0;
+        padding: 0;
     }
     
     body {
