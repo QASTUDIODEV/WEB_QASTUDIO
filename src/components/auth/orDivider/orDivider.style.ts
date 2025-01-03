2bb2d4a192ea184ac41b4a075ed1815c04946e89
김연진 <kyj0719@gmail.com>
2025-01-03 20:10:53 +0900

    feat: 로그인, 회원가입 UI 1차 구현

---
+++ src/components/auth/orDivider/orDivider.style.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const Line = styled.line`
+  background-color: ${({ theme }) => theme.colors.gray.gray_500};
+  width: 180px;
+  height: 0.1px;
+  gap: 0px;
+  border: 1px 0px 0px 0px;
+  position: absolute;
+`;
+
+const Container = styled.div`
+  ${({ theme }) => theme.align.row_center}
+  position: relative;
+  width: 180px;
+  height: 10px;
+  span {
+    ${({ theme }) => theme.align.row_center};
+    ${({ theme }) => theme.text.medium_14};
+    color: ${({ theme }) => theme.colors.gray.gray_300};
+    z-index: 1;
+    width: 60px;
+    background-color: ${({ theme }) => theme.colors.black};
+  }
+`;
+
+export { Container, Line };
--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -1,11 +1,74 @@
+import { useState } from 'react';
+
+import OrDivider from '@/components/auth/orDivider/orDivider';
+import SocialLogo from '@/components/auth/socialLogo/socialLogo';
 import Input from '@/components/common/input/input.tsx';
+import Profile from '@/components/common/profile/profile';
+
+import ArrowLeft from '@/image/arrow_left.svg';
+import defaultProfile from '@/image/defaultProfile.svg';
+import Logo from '@/image/logo.svg';
+import ProfileEdit from '@/image/profileEdit.svg';
 
 import * as S from '@/pages/signup/signup.style';
 
 export default function SignupPage() {
+  const [step, setStep] = useState(0);
   return (
     <S.Container>
-      <Input />
+      <img src={Logo} alt="" />
+      {step === 0 && (
+        <>
+          <S.Form>
+            <S.Wrapper>
+              <span>Email</span>
+              <Input />
+              {/* 임시 버튼 */}
+              <button style={{ position: 'absolute', right: '-50px', top: '25px' }}>Verify</button>
+            </S.Wrapper>
+
+            <S.Wrapper>
+              <Input />
+            </S.Wrapper>
+
+            <S.Wrapper>
+              <span>Password</span>
+              <Input />
+            </S.Wrapper>
+            {/* 임시 버튼 */}
+            <button onClick={() => setStep(1)}>Sign up</button>
+          </S.Form>
+
+          <OrDivider />
+          <SocialLogo />
+          <S.BackButton>
+            <img src={ArrowLeft} />
+            Back
+          </S.BackButton>
+        </>
+      )}
+      {step === 1 && (
+        <>
+          <S.ProfileImg>
+            {/* <Profile profileImg={defaultProfile} /> */}
+            <S.ProfileEditBtn>
+              <img src={ProfileEdit} />
+            </S.ProfileEditBtn>
+          </S.ProfileImg>
+
+          <S.Form>
+            <span>Nickname</span>
+            <Input />
+            {/* 임시 버튼 */}
+            <button onClick={() => setStep(1)}>Sign up</button>
+          </S.Form>
+
+          <S.BackButton>
+            <img src={ArrowLeft} />
+            Back
+          </S.BackButton>
+        </>
+      )}
     </S.Container>
   );
 }
