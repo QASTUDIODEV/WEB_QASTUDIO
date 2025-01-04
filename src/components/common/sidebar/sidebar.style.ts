f8e50603dcff09d823784b9c1392bd34b00d5ef1
lwittyl <mercury0502@dgu.ac.kr>
2025-01-04 15:08:04 +0900

    feat: 사이드바 UI 구현

--- src/components/common/sidebar/sidebar.style.ts
+++ src/components/common/sidebar/sidebar.style.ts
@@ -1,5 +1,143 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const SideBar = styled.div`
+  width: 320px;
+  height: 100vh;
+  background: ${({ theme }) => theme.colors.primary.pri_800};
+  position: fixed;
+  overflow-y: auto;
+  color: ${({ theme }) => theme.colors.gray.gray_50};
+`;
 
-export { Container };
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 50px;
+  padding-left: 40px;
+`;
+
+const Profile = styled.div`
+  margin-top: 30px;
+  padding: 12px 0;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const ProfileImg = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const ProfileName = styled.p`
+  margin: 0;
+  ${({ theme }) => theme.text.medium_24};
+  color: ${({ theme }) => theme.colors.gray.gray_50};
+`;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 0;
+  gap: 8px;
+  cursor: pointer;
+`;
+
+const SearchText = styled.p`
+  margin: 0;
+  ${({ theme }) => theme.text.medium_18};
+  color: ${({ theme }) => theme.colors.gray.gray_50};
+`;
+
+const SearchInput = styled.input`
+  margin: 12px 0;
+  width: 240px;
+  height: 47px;
+  padding: 14px 17px;
+  border-radius: 8.57px;
+  border: 1px solid ${({ theme }) => theme.colors.white};
+  background: none;
+  color: ${({ theme }) => theme.colors.white};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray.gray_200};
+  }
+`;
+
+const Projects = styled.div`
+  margin-top: 32px;
+  padding: 12px 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+`;
+
+const ProjectText = styled.p`
+  margin: 0;
+  ${({ theme }) => theme.text.medium_24};
+  color: ${({ theme }) => theme.colors.gray.gray_50};
+`;
+
+const Project = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 40px;
+  gap: 8px;
+  cursor: pointer;
+`;
+
+const ProjectName = styled.p`
+  margin: 0;
+  ${({ theme }) => theme.text.medium_18};
+  color: ${({ theme }) => theme.colors.gray.gray_50};
+`;
+
+const ProjectContent = styled.div<{ $isActive?: boolean }>`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 12px 40px 12px 60px;
+  background: ${({ $isActive }) => ($isActive ? '#424854CC' : 'transparent')};
+`;
+
+const ProjectContentName = styled.p`
+  margin: 0;
+  ${({ theme }) => theme.text.medium_18};
+  color: ${({ theme }) => theme.colors.gray.gray_50};
+`;
+
+const ProjectContents = styled.div<{ $isOpen: boolean }>`
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  max-height: ${(props) => (props.$isOpen ? '300px' : '0')};
+  transition: max-height 0.3s ease-out;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: inherit;
+`;
+
+export {
+  Container,
+  Profile,
+  ProfileImg,
+  ProfileName,
+  Project,
+  ProjectContent,
+  ProjectContentName,
+  ProjectContents,
+  ProjectName,
+  Projects,
+  ProjectText,
+  Search,
+  SearchInput,
+  SearchText,
+  SideBar,
+  StyledNavLink,
+};
--- src/components/common/sidebar/sidebar.tsx
+++ src/components/common/sidebar/sidebar.tsx
@@ -1,5 +1,89 @@
+import { useState } from 'react';
+
 import * as S from '@/components/common/sidebar/sidebar.style';
 
+import ArrowDown from '@/assets/images/ArrowDown.svg?react';
+import DashboardLogo from '@/assets/images/DashboardLogo.svg?react';
+import InformationLogo from '@/assets/images/InformationLogo.svg?react';
+import Logo from '@/assets/images/Logo.svg?react';
+import Plus from '@/assets/images/Plus.svg?react';
+import ProjectLogo from '@/assets/images/ProjectLogo.svg?react';
+import SearchImg from '@/assets/images/SearchImg.svg?react';
+import SenarioLogo from '@/assets/images/SenarioLogo.svg?react';
+
 export default function Sidebar() {
-  return <S.Container />;
+  const [searchActive, setSearchActive] = useState(false);
+  const [menuStates, setMenuStates] = useState([false, false, false]);
+
+  const toggleMenu = (index: number) => {
+    setMenuStates((prevStates) => prevStates.map((state, i) => (i === index ? !state : state)));
+  };
+
+  const projects = [
+    { id: 1, name: 'UMC_PM_DAY' },
+    { id: 2, name: 'Project_2' },
+    { id: 3, name: 'Project_3' },
+  ];
+
+  return (
+    <S.SideBar>
+      <S.Container>
+        <Logo />
+        <S.Profile>
+          <S.ProfileImg />
+          <S.ProfileName>eunji</S.ProfileName>
+        </S.Profile>
+        <S.Search
+          onClick={() => {
+            setSearchActive(!searchActive);
+          }}
+        >
+          <SearchImg />
+          <S.SearchText>검색</S.SearchText>
+        </S.Search>
+        {searchActive && <S.SearchInput placeholder="검색어" />}
+      </S.Container>
+
+      <S.Projects>
+        <S.ProjectText>Projects</S.ProjectText>
+        <Plus />
+      </S.Projects>
+
+      {projects.map((project, index) => (
+        <div key={project.id}>
+          <S.Project onClick={() => toggleMenu(index)}>
+            <ProjectLogo />
+            <S.ProjectName>{project.name}</S.ProjectName>
+            <ArrowDown />
+          </S.Project>
+          <S.ProjectContents $isOpen={menuStates[index]}>
+            <S.StyledNavLink to={`/project/dashboard/${project.id}`}>
+              {({ isActive }) => (
+                <S.ProjectContent $isActive={isActive}>
+                  <DashboardLogo />
+                  <S.ProjectContentName>Dashboard</S.ProjectContentName>
+                </S.ProjectContent>
+              )}
+            </S.StyledNavLink>
+            <S.StyledNavLink to={`/project/information/${project.id}`}>
+              {({ isActive }) => (
+                <S.ProjectContent $isActive={isActive}>
+                  <InformationLogo />
+                  <S.ProjectContentName>Information</S.ProjectContentName>
+                </S.ProjectContent>
+              )}
+            </S.StyledNavLink>
+            <S.StyledNavLink to={`/project/scenario/${project.id}`}>
+              {({ isActive }) => (
+                <S.ProjectContent $isActive={isActive}>
+                  <SenarioLogo />
+                  <S.ProjectContentName>Scenario</S.ProjectContentName>
+                </S.ProjectContent>
+              )}
+            </S.StyledNavLink>
+          </S.ProjectContents>
+        </div>
+      ))}
+    </S.SideBar>
+  );
 }
