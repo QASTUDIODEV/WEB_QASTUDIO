8fc0a2dd85a026e339a6e440245677326e67d9bb
lwittyl <mercury0502@dgu.ac.kr>
2025-01-05 20:42:29 +0900

    feat: Information 페이지 UI 초기 구현 #17

--- src/pages/projectInfo/projectInfo.style.ts
+++ src/pages/projectInfo/projectInfo.style.ts
@@ -1,5 +1,112 @@
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  color: #d6deec;
+  background: var(--primary-pri_900, #000714);
+  width: 100%;
+  flex: 1;
+`;
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.813rem;
+`;
 
-export { Container };
+const ProfileImg = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const ProfileName = styled.p`
+  font-family: Pretendard;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: 0.48px;
+`;
+
+const Box = styled.div<{ height: string; padding: string }>`
+  height: ${(props) => props.height};
+  background: var(--primary-pri_back_grad, linear-gradient(76deg, #0e2245 0%, #000714 100.13%));
+  border-radius: 8px;
+  padding: ${(props) => props.padding};
+  position: relative;
+`;
+
+const Left = styled.div`
+  width: 75%;
+  gap: 4.310344%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Right = styled.div`
+  width: 22.06%;
+  gap: 4.310344%;
+  margin-top: 0;
+  display: flex;
+  flex-direction: column;
+  margin-left: 2.94112%;
+`;
+
+const BoxContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 6.375% 7.5% 3.125% 7.5%;
+  display: flex;
+`;
+
+const Title = styled.p`
+  color: var(--primary-pri_50, #dfe8f9);
+  font-family: Pretendard;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: 0.44px;
+`;
+
+const Text = styled.p`
+  color: var(--gray-gray_300, #999);
+  padding-top: 1.960784%;
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%;
+  letter-spacing: 0.36px;
+`;
+
+const TextBtn = styled.button<{ width: string; height: string; color?: string; padding?: string }>`
+  display: flex;
+  gap: 3.53356%;
+  align-items: center;
+  justify-content: space-around;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  position: absolute;
+  bottom: 5%;
+  right: 3.23276%;
+  background: ${(props) => props.color || 'rgba(223, 232, 249, 0.1)'};
+  padding: ${(props) => props.padding || '0.98039% 1.960784%'};
+  border-radius: 8px;
+  border: none;
+
+  .text {
+    color: #dfe8f9;
+    font-family: Pretendard;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 150%;
+    letter-spacing: 0.36px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export { Box, BoxContainer, Container, Left, Profile, ProfileImg, ProfileName, Right, Text, TextBtn, Title };
--- src/pages/projectInfo/projectInfo.tsx
+++ src/pages/projectInfo/projectInfo.tsx
@@ -1,11 +1,68 @@
-import Input from '@/components/common/input/input.tsx';
-
+import Plus from '@/assets/icons/add.svg?react';
+import Locate from '@/assets/icons/location_arrow.svg?react';
+import Upload from '@/assets/icons/upload.svg?react';
 import * as S from '@/pages/projectInfo/projectInfo.style';
 
 export default function ProjectInfoPage() {
   return (
     <S.Container>
-      <Input />
+      <S.BoxContainer>
+        <S.Left>
+          <S.Profile>
+            <S.ProfileImg />
+            <S.ProfileName>ProjectName</S.ProfileName>
+          </S.Profile>
+          <S.Box height="17.2413%" padding="2.155172% 3.23276%">
+            <S.Title>Introduction to the Project</S.Title>
+            <S.Text>Please upload the project</S.Text>
+          </S.Box>
+          <S.Box height="43.1034%" padding="2.155172% 3.23276%">
+            <S.Title>Add Project File</S.Title>
+            <S.Text>
+              Please enter the project folder for AI to understand the project.
+              <br />
+              Please compress and enter the React JS/TS file.
+            </S.Text>
+            <S.TextBtn width="27.7409%" height="11.75%" color="#0D409D">
+              <Upload />
+              <span className="text">Upload Project File (.zip)</span>
+            </S.TextBtn>
+          </S.Box>
+          <S.Box height="23.0603%" padding="2.155172% 3.23276%">
+            <S.Title>Add Character</S.Title>
+            <S.Text>
+              Please set up a user character to use the service.
+              <br />
+              AI automatically creates appropriate scenarios.
+            </S.Text>
+            <S.TextBtn width="25.88235294%" height="21.96261%">
+              <Plus />
+              <span className="text">Create New Character</span>
+            </S.TextBtn>
+          </S.Box>
+        </S.Left>
+        <S.Right>
+          <S.Profile>
+            <S.ProfileImg />
+            <S.ProfileName>ProjectName</S.ProfileName>
+          </S.Profile>
+          <S.Box height="64.65518%" padding="5.882352% 8.82352%">
+            <S.Title>Team Members</S.Title>
+            <S.TextBtn width="38.82352941%" height="7.833333333%" padding="2.941176471% 5.882352941%">
+              <Plus />
+              <span className="text">Invite</span>
+            </S.TextBtn>
+          </S.Box>
+          <S.Box height="23.06034%" padding="5.882352% 8.82352%">
+            <S.Title>View QA Sheet</S.Title>
+            <S.Text>Check the QA sheet</S.Text>
+            <S.TextBtn width="40.33333333%" height="21.96261%" padding="2.941176471% 5.882352941%">
+              <Locate />
+              <span className="text">Locate</span>
+            </S.TextBtn>
+          </S.Box>
+        </S.Right>
+      </S.BoxContainer>
     </S.Container>
   );
 }
