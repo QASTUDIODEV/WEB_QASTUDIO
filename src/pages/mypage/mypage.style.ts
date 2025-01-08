789622408c22d43926e15462f03016c781c9da55
김연진 <kyj0719@gmail.com>
2025-01-08 14:31:38 +0900

    feat: 기본 틀 잡기

--- src/pages/mypage/mypage.style.ts
+++ src/pages/mypage/mypage.style.ts
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+`;
+const ProfileWrapper = styled.div`
+  display: flex;
+`;
 
-export { Container };
+const UserInfo = styled.div`
+  display: flex;
+`;
+const Account = styled.div`
+  display: flex;
+`;
+export { Account, Container, ProfileWrapper, UserInfo };
--- src/pages/mypage/mypage.tsx
+++ src/pages/mypage/mypage.tsx
@@ -1,5 +1,18 @@
+import Profile from '@/components/common/profile/profile';
+
 import * as S from '@/pages/mypage/mypage.style';
 
 export default function MyPage() {
-  return <S.Container>{/*<Input />*/}</S.Container>;
+  return (
+    <S.Container>
+      <span>My Page</span>
+      <S.ProfileWrapper>
+        <Profile />
+        <S.UserInfo>
+          <span>eunji</span>
+          <S.Account>email.email.com</S.Account>
+        </S.UserInfo>
+      </S.ProfileWrapper>
+    </S.Container>
+  );
 }
