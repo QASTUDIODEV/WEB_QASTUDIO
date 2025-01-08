988438b9a4d90bb606f27dd3d29de3e532c5f80f
lwittyl <mercury0502@dgu.ac.kr>
2025-01-08 21:20:56 +0900

    feat: UI 2차 구현 #17

--- src/pages/projectInfo/projectInfo.tsx
+++ src/pages/projectInfo/projectInfo.tsx
@@ -1,6 +1,8 @@
+import Button from '@/components/common/button/button';
+
 import Plus from '@/assets/icons/add.svg?react';
-import Locate from '@/assets/icons/location_arrow.svg?react';
-import Upload from '@/assets/icons/upload.svg?react';
+import Goto from '@/assets/icons/arrow_goto.svg?react';
+import Edit from '@/assets/icons/edit.svg?react';
 import * as S from '@/pages/projectInfo/projectInfo.style';
 
 export default function ProjectInfoPage() {
@@ -8,7 +10,12 @@ export default function ProjectInfoPage() {
     <S.Container>
       <S.Profile>
         <S.ProfileImg />
-        <S.ProfileName>ProjectName</S.ProfileName>
+        <S.ProfileName>UMC_PM_DAY</S.ProfileName>
+        <S.Wrapper top="5.6px" right="0">
+          <Button type="normal" color="default" icon={<Goto />} iconPosition="right">
+            Go to Site
+          </Button>
+        </S.Wrapper>
       </S.Profile>
       <S.Box height="18%">
         <S.Title>Introduction to the Project</S.Title>
@@ -16,41 +23,46 @@ export default function ProjectInfoPage() {
           사용자가 학습 로드맵을 생성하고 이를 직관적으로 확인할 수 있도록 지원합니다. <br />
           (두줄까지 들어갈 수 있습니다.)
         </S.Text>
+        <S.Wrapper bottom="16px" right="24px">
+          <Button type="normal" color="default" icon={<Edit />} iconPosition="left">
+            Edit
+          </Button>
+        </S.Wrapper>
       </S.Box>
       <S.SemiBox>
         <S.Left>
           <S.Box height="64.19117647058824%">
-            <S.Title>Add Project File</S.Title>
-            <S.Text>
-              Please enter the project folder for AI to understand the project.
+            <S.Title>Project structure</S.Title>
+            <S.TextBold>Summary</S.TextBold>
+            <S.TextLight>
+              사용자가 학습 로드맵을 생성하고 이를 직관적으로 확인할 수 있도록 지원합니다.
               <br />
-              Please compress and enter the React JS/TS file.
-            </S.Text>
-            <S.TextBtn width="27.7409%" height="11.75%" color="#0D409D">
-              <Upload />
-              <span className="text">Upload Project File (.zip)</span>
-            </S.TextBtn>
+              (두줄까지 들어갈 수 있습니다.)
+            </S.TextLight>
+            <S.Wrapper top="16px" right="24px">
+              <Plus />
+            </S.Wrapper>
+            <S.InnerBox>
+              <S.Wrapper top="5.6px" right="8px">
+                <Plus />
+              </S.Wrapper>
+            </S.InnerBox>
           </S.Box>
           <S.Box height="33.6%">
-            <S.Title>Add Character</S.Title>
-            <S.Text>
-              Please set up a user character to use the service.
-              <br />
-              AI automatically creates appropriate scenarios.
-            </S.Text>
-            <S.TextBtn width="25.88235294%" height="21.96261%">
+            <S.Title>Character</S.Title>
+            <S.CharacterAddBox>
               <Plus />
-              <span className="text">Create New Character</span>
-            </S.TextBtn>
+            </S.CharacterAddBox>
           </S.Box>
         </S.Left>
         <S.Right>
           <S.Box height="100%">
             <S.Title>Team Members</S.Title>
-            <S.TextBtn width="38.82352941%" height="7.833333333%">
-              <Plus />
-              <span className="text">Invite</span>
-            </S.TextBtn>
+            <S.Wrapper bottom="16px" right="24px">
+              <Button type="normal" color="default" icon={<Plus />} iconPosition="left">
+                Invite
+              </Button>
+            </S.Wrapper>
           </S.Box>
         </S.Right>
       </S.SemiBox>
