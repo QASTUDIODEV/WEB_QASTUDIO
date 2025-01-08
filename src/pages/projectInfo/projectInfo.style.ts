795ea05c0f4523b0cac3143b883ac90c24d09276
lwittyl <mercury0502@dgu.ac.kr>
2025-01-08 21:20:56 +0900

    feat: UI 2차 구현 #17

--- src/pages/projectInfo/projectInfo.style.ts
+++ src/pages/projectInfo/projectInfo.style.ts
@@ -14,6 +14,8 @@ const Profile = styled.div`
   display: flex;
   align-items: center;
   gap: 0.813rem;
+  margin-bottom: 33.6px;
+  position: relative;
 `;
 
 const ProfileImg = styled.img`
@@ -38,6 +40,8 @@ const Box = styled.div<{ height: string }>`
   border-radius: 8px;
   padding: 16px 24px;
   position: relative;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Left = styled.div`
@@ -64,6 +68,17 @@ const Title = styled.p`
   font-weight: 500;
   line-height: 150%;
   letter-spacing: 0.352px;
+  margin-bottom: 16px;
+`;
+
+const TextLight = styled.p`
+  color: ${({ theme }) => theme.colors.white};
+  font-family: Pretendard;
+  font-size: 11.2px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%; /* 16.8px */
+  letter-spacing: 0.224px;
 `;
 
 const Text = styled.p`
@@ -76,33 +91,15 @@ const Text = styled.p`
   letter-spacing: 0.288px;
 `;
 
-const TextBtn = styled.button<{ width: string; height: string; color?: string; padding?: string }>`
-  display: flex;
-  gap: 3.53356%;
-  align-items: center;
-  justify-content: space-around;
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
-  position: absolute;
-  bottom: 5%;
-  right: 3.23276%;
-  background: ${(props) => props.color || 'rgba(223, 232, 249, 0.1)'};
-  padding: ${(props) => props.padding || '0.98039% 1.960784%'};
-  border-radius: 8px;
-  border: none;
-
-  .text {
-    color: #dfe8f9;
-    font-family: Pretendard;
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 150%;
-    letter-spacing: 0.36px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
+const TextBold = styled.p`
+  color: ${({ theme }) => theme.colors.primary.pri_50};
+  font-family: Pretendard;
+  font-size: 22.4px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%;
+  letter-spacing: 0.448px;
+  margin-bottom: 8px;
 `;
 
 const SemiBox = styled.div`
@@ -111,4 +108,48 @@ const SemiBox = styled.div`
   margin-top: 32px;
 `;
 
-export { Box, Container, Left, Profile, ProfileImg, ProfileName, Right, SemiBox, Text, TextBtn, Title };
+const InnerBox = styled.div`
+  box-sizing: border-box;
+  background: rgba(217, 230, 255, 0.05);
+  border-radius: 6.4px;
+  padding: 5.6px 8px;
+  position: relative;
+  margin-top: 16px;
+  height: 100%;
+`;
+
+const CharacterAddBox = styled.div`
+  background: #d9e6ff1a;
+  border-radius: 6.4px;
+  height: 100%;
+  width: 13.7254902%;
+  ${({ theme }) => theme.align.row_center}
+
+  svg {
+    width: 50%;
+    height: 50%;
+  }
+`;
+const CharacterBox = styled.div`
+  background: ${({ theme }) => theme.colors.point.point_1};
+  border-radius: 6.4px;
+  height: 100%;
+  width: 13.7254902%;
+  padding: 8px 12px;
+`;
+type TWrapperProps = {
+  top?: string;
+  bottom?: string;
+  right?: string;
+  left?: string;
+};
+
+export const Wrapper = styled.div<TWrapperProps>`
+  position: absolute;
+  top: ${(props) => props.top};
+  bottom: ${(props) => props.bottom};
+  left: ${(props) => props.left};
+  right: ${(props) => props.right};
+`;
+
+export { Box, CharacterAddBox, CharacterBox, Container, InnerBox, Left, Profile, ProfileImg, ProfileName, Right, SemiBox, Text, TextBold, TextLight, Title };
