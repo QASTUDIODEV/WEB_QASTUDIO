675062b1d4e77b9526200585a34f041d244a01e5
hijjoy <hijjoy@naver.com>
2025-01-12 15:32:39 +0900

    feat: 대시보드 페이지 구현(표 제외) #33

--- src/pages/dashboard/dashboard.style.ts
+++ src/pages/dashboard/dashboard.style.ts
@@ -1,14 +1,54 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
   width: 100%;
   height: 100%;
+
+  padding: 50px 50px;
 `;
 
-const Flex = styled.div`
-  ${({ theme }) => theme.align.column_start};
+const ProjectTitleBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  p {
+    ${({ theme }) => theme.text.medium_18};
+  }
+`;
+
+const ProfileBox = styled.div`
+  width: 30px;
+  height: 30px;
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+const TableWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   gap: 15px;
-  padding-top: 20px;
+
+  span {
+    width: 100%;
+    height: 400px;
+    background-color: antiquewhite;
+  }
+
+  img {
+    width: 100%;
+  }
 `;
 
-export { Container, Flex };
+const SearchBox = styled.div`
+  width: 180px;
+`;
+
+export { Container, InfoWrapper, ProfileBox, ProjectTitleBox, SearchBox, TableWrapper };
--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,30 +1,47 @@
-import { useDispatch, useSelector } from '@/hooks/common/useCustomRedux.ts';
+import Profile from '@/components/common/profile/profile';
+import SearchBar from '@/components/common/searchBar/searchBar';
+import PercentLabel from '@/components/dashboard/percentLabel/percentLabel';
+import ProjectInfoBox from '@/components/dashboard/projectInfoBox/projectInfoBox';
 
-import Modal from '@/components/common/modal/modal.tsx';
-
+import FailIcon from '@/assets/icons/fail.svg?react';
+import StarIcon from '@/assets/icons/star.svg?react';
+import SuccessIcon from '@/assets/icons/success.svg?react';
+import UserIcon from '@/assets/icons/user_profile.svg?react';
 import * as S from '@/pages/dashboard/dashboard.style';
-import { setOpen } from '@/slices/modalSlice.ts';
 
 export default function DashboardPage() {
-  const { isOpen } = useSelector((state) => state);
-  const dispatch = useDispatch();
+  const title = 'UMC_PM_DAY';
 
-  /*예시 모달*/
   return (
     <S.Container>
-      <button onClick={() => dispatch(setOpen())}>클릭</button>
-      {isOpen && (
-        <Modal title={'UMC_PM_DAY'} onClose={() => dispatch(setOpen())}>
-          <S.Flex>
-            <div>
-              Define users for the registered project.
-              <br />
-              QASTUDIO will create a suitable scenario for you.
-            </div>
-            <button onClick={() => {}}>테스트</button>
-          </S.Flex>
-        </Modal>
-      )}
+      <S.ProjectTitleBox>
+        <S.ProfileBox>
+          <Profile />
+        </S.ProfileBox>
+        <p>{title}</p>
+      </S.ProjectTitleBox>
+      <S.InfoWrapper>
+        <ProjectInfoBox
+          label={'Successes'}
+          content={'500'}
+          icon={<SuccessIcon width={'14px'} height={'14px'} />}
+          percentLabel={<PercentLabel percent={24.8} increase={true} />}
+        />
+        <ProjectInfoBox
+          label={'Failures'}
+          content={'23'}
+          icon={<FailIcon width={'14px'} height={'14px'} />}
+          percentLabel={<PercentLabel percent={17.8} increase={false} />}
+        />
+        <ProjectInfoBox label={'Participants'} content={'5'} icon={<UserIcon width={'15px'} height={'15px'} />} />
+        <ProjectInfoBox label={'Total Test'} content={'15'} icon={<StarIcon />} />
+      </S.InfoWrapper>
+      <S.TableWrapper>
+        <S.SearchBox>
+          <SearchBar placeholder={'Search by name'} />
+        </S.SearchBox>
+        {/*<span>표자리</span>*/}
+      </S.TableWrapper>
     </S.Container>
   );
 }
