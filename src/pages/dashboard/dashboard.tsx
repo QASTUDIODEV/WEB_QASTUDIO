2d65242ca745a8b18ae8f5a293ff0300ad237aa6
hijjoy <hijjoy@naver.com>
2025-01-13 22:44:23 +0900

    feat: 대시보드 구현 #33

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,6 +2,7 @@ import Profile from '@/components/common/profile/profile';
 import SearchBar from '@/components/common/searchBar/searchBar';
 import PercentLabel from '@/components/dashboard/percentLabel/percentLabel';
 import ProjectInfoBox from '@/components/dashboard/projectInfoBox/projectInfoBox';
+import Table from '@/components/dashboard/table/table';
 
 import FailIcon from '@/assets/icons/fail.svg?react';
 import StarIcon from '@/assets/icons/star.svg?react';
@@ -14,34 +15,37 @@ export default function DashboardPage() {
 
   return (
     <S.Container>
+      {/*TODO: 프로필 box 공용컴포넌트로 수정 예정*/}
       <S.ProjectTitleBox>
         <S.ProfileBox>
           <Profile />
         </S.ProfileBox>
         <p>{title}</p>
       </S.ProjectTitleBox>
-      <S.InfoWrapper>
-        <ProjectInfoBox
-          label={'Successes'}
-          content={'500'}
-          icon={<SuccessIcon width={'14px'} height={'14px'} />}
-          percentLabel={<PercentLabel percent={24.8} increase={true} />}
-        />
-        <ProjectInfoBox
-          label={'Failures'}
-          content={'23'}
-          icon={<FailIcon width={'14px'} height={'14px'} />}
-          percentLabel={<PercentLabel percent={17.8} increase={false} />}
-        />
-        <ProjectInfoBox label={'Participants'} content={'5'} icon={<UserIcon width={'15px'} height={'15px'} />} />
-        <ProjectInfoBox label={'Total Test'} content={'15'} icon={<StarIcon />} />
-      </S.InfoWrapper>
-      <S.TableWrapper>
-        <S.SearchBox>
-          <SearchBar placeholder={'Search by name'} />
-        </S.SearchBox>
-        {/*<span>표자리</span>*/}
-      </S.TableWrapper>
+      <S.ContentWrapper>
+        <S.InfoWrapper>
+          <ProjectInfoBox
+            label={'Successes'}
+            content={'500'}
+            icon={<SuccessIcon width={'14px'} height={'14px'} />}
+            percentLabel={<PercentLabel percent={24.8} increase={true} />}
+          />
+          <ProjectInfoBox
+            label={'Failures'}
+            content={'23'}
+            icon={<FailIcon width={'14px'} height={'14px'} />}
+            percentLabel={<PercentLabel percent={17.8} increase={false} />}
+          />
+          <ProjectInfoBox label={'Participants'} content={'5'} icon={<UserIcon width={'15px'} height={'15px'} />} />
+          <ProjectInfoBox label={'Total Test'} content={'15'} icon={<StarIcon />} />
+        </S.InfoWrapper>
+        <S.TableWrapper>
+          <S.SearchBox>
+            <SearchBar placeholder={'Search by name'} />
+          </S.SearchBox>
+          <Table />
+        </S.TableWrapper>
+      </S.ContentWrapper>
     </S.Container>
   );
 }
