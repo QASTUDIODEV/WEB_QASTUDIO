2027eb0b640aa5752538d4e51b89207526db288f
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-01-15 04:33:08 +0900

    chore: 아이콘 추가

--- src/components/common/projectTitle/projectTitle.tsx
+++ src/components/common/projectTitle/projectTitle.tsx
@@ -1,7 +1,10 @@
 import Profile from '@/components/common/profile/profile';
 import * as S from '@/components/common/projectTitle/projectTitle.style';
 
+import JSIcon from '@/assets/icons/js.svg?react';
 import Mobile from '@/assets/icons/mobile.svg?react';
+import NextJSIcon from '@/assets/icons/nextjs.svg?react';
+import ReactIcon from '@/assets/icons/react.svg?react';
 import Web from '@/assets/icons/web.svg?react';
 
 type TModalProps = { title?: string; profileImg?: string; device?: string; stack?: string };
@@ -14,7 +17,7 @@ export default function ProjectTitle({ title = 'UNTITLED', profileImg = undefine
       </S.ProfileWrapper>
       <p>{title}</p>
       {device == 'pc' ? <Web /> : device == 'mobile' ? <Mobile /> : <Web />}
-      {stack == 'next.js' ? <Web /> : stack == 'react' ? <Web /> : stack == 'js' ? <Web /> : <Web />}
+      {stack == 'next.js' ? <NextJSIcon /> : stack == 'react' ? <ReactIcon /> : stack == 'js' ? <JSIcon /> : <NextJSIcon />}
     </S.Container>
   );
 }
--- src/pages/scenario/scenario.tsx
+++ src/pages/scenario/scenario.tsx
@@ -1,4 +1,5 @@
 import Input from '@/components/common/input/input.tsx';
+import ProjectTitle from '@/components/common/projectTitle/projectTitle';
 
 import * as S from '@/pages/scenario/scenario.style';
 
@@ -6,6 +7,7 @@ export default function ScenarioPage() {
   return (
     <S.Container>
       <Input />
+      <ProjectTitle title="My Mobile App" profileImg="/icons/logo.svg" device="pc" stack="js" />
     </S.Container>
   );
 }
