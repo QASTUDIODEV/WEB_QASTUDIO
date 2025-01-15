826ebe9099bc39c0ff3f3ca0860b9f3ae3c51ed5
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-01-15 22:22:54 +0900

    refactor: projectTitle 아이콘 맵핑 방식 수정

--- src/components/common/projectTitle/projectTitle.tsx
+++ src/components/common/projectTitle/projectTitle.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Profile from '@/components/common/profile/profile';
 import * as S from '@/components/common/projectTitle/projectTitle.style';
 
@@ -7,21 +9,54 @@ import NextJSIcon from '@/assets/icons/nextjs.svg?react';
 import ReactIcon from '@/assets/icons/react.svg?react';
 import Web from '@/assets/icons/web.svg?react';
 
-type TModalProps = { title?: string; profileImg?: string; device?: string; stack?: string };
+type TProjectTitleProps = {
+  title?: string;
+  profileImg?: string;
+  device: DEVICE;
+  stack: STACK;
+};
 
-export default function ProjectTitle({ title = 'UNTITLED', profileImg = undefined, device = 'pc', stack = 'next.js' }: TModalProps) {
+export enum STACK {
+  NEXT = 'next',
+  REACT = 'react',
+  JS = 'js',
+}
+
+export enum DEVICE {
+  PC = 'pc',
+  MOBILE = 'mobile',
+}
+
+// 아이콘 맵핑
+const stackIconMap: Record<string, React.FunctionComponent> = {
+  [STACK.NEXT]: NextJSIcon,
+  [STACK.REACT]: ReactIcon,
+  [STACK.JS]: JSIcon,
+};
+
+const deviceIconMap: Record<string, React.FunctionComponent> = {
+  [DEVICE.PC]: Web,
+  [DEVICE.MOBILE]: Mobile,
+};
+
+// 아이콘 반환 함수
+function getIcon(map: Record<string, React.FunctionComponent>, key: string): React.ReactNode {
+  const Icon = map[key];
+  return Icon ? <Icon /> : null;
+}
+
+export default function ProjectTitle({ title = 'UNTITLED', profileImg, device = DEVICE.PC, stack = STACK.NEXT }: TProjectTitleProps) {
   return (
     <S.Container>
       <S.ProfileWrapper>
         <Profile profileImg={profileImg} />
       </S.ProfileWrapper>
       <p>{title}</p>
-      {device == 'pc' ? <Web /> : device == 'mobile' ? <Mobile /> : <Web />}
-      {stack == 'next.js' ? <NextJSIcon /> : stack == 'react' ? <ReactIcon /> : stack == 'js' ? <JSIcon /> : <NextJSIcon />}
+      {getIcon(deviceIconMap, device)}
+      {getIcon(stackIconMap, stack)}
     </S.Container>
   );
 }
 
-// 사용 예시:
-// import ProjectTitle from '@/components/common/projectTitle/projectTitle';
-// <ProjectTitle title="UMC_PM_DAY" profileImg="/icons/logo.svg" device="mobile" stack="react" />
+//사용예시:
+//<ProjectTitle title="UMC_PM_DAY" device={DEVICE.MOBILE} stack={STACK.JS} />
