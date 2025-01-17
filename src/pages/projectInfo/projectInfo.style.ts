8e2b547b8080122c9303db8a1a769eecb31de95d
lwittyl <mercury0502@dgu.ac.kr>
2025-01-18 00:06:30 +0900

    feat: table 기능 구현

--- src/pages/projectInfo/projectInfo.style.ts
+++ src/pages/projectInfo/projectInfo.style.ts
@@ -309,6 +309,36 @@ export const AccessRights = styled.div`
   gap: 10px;
   align-items: center;
 `;
+export const ModalBox = styled.div`
+  gap: 20px;
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+`;
+export const PostBox = styled.div`
+  gap: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+export const ModalText = styled.p`
+  ${({ theme }) => theme.text.medium_22};
+  color: ${({ theme }) => theme.colors.primary.pri_50};
+`;
+export const BtnWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+`;
+export const ProjectText = styled.p`
+  ${({ theme }) => theme.text.medium_24};
+  color: #d6deec;
+`;
+export const Position = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
 export {
   Box,
   Character,
