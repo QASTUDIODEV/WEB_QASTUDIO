03c497a0a8df63297e3d2fff77e473c6e88c74fb
김연진 <kyj0719@gmail.com>
2025-01-19 23:14:39 +0900

    fix: build 에러 해결

--- src/main.tsx
+++ src/main.tsx
@@ -18,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
           <ReactQueryDevtools initialIsOpen={false} />
+          <GlobalStyle />
           <App />
         </ThemeProvider>
       </QueryClientProvider>
