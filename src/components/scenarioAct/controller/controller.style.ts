a69ab115a4a1758f06d55df991e9a194354a0d21
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-01-20 21:54:23 +0900

    feat: react-frame-component test구현, 시나리오 실행 UI 프레임

--- src/components/scenarioAct/controller/controller.style.ts
+++ src/components/scenarioAct/controller/controller.style.ts
@@ -1,9 +1,28 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background: ${({ theme }) => theme.colors.primary.pri_900}
+  position: relative;
+  background: ${({ theme }) => theme.colors.primary.pri_900};
   box-shadow: 10px 20px 100px 0px rgba(35, 104, 232, 0.2);
   color: ${({ theme }) => theme.colors.primary.pri_50};
   ${({ theme }) => theme.text.medium_20};
   height: 100%;
+`;
+export const Header = styled.div`
+  padding: 20px 30px;
+  display: flex;
+  justify-content: space-between;
+  ${({ theme }) => theme.text.bold_24};
+`;
+export const CharacterHeader = styled.div`
+  padding: 10px 30px;
+  display: flex;
+  justify-content: space-between;
+  ${({ theme }) => theme.text.medium_24};
+`;
+
+export const ButtonContainer = styled.div`
+  position: absolute;
+  bottom: 31px;
+  right: 30px;
 `;
--- src/components/scenarioAct/controller/controller.tsx
+++ src/components/scenarioAct/controller/controller.tsx
@@ -1,5 +1,26 @@
+import Button from '@/components/common/button/button';
 import * as S from '@/components/scenarioAct/controller/controller.style';
 
+import Add from '@/assets/icons/add.svg?react';
+import Delete from '@/assets/icons/delete.svg?react';
+
 export default function Controller() {
-  return <S.Container>컨트롤러</S.Container>;
+  return (
+    <S.Container>
+      <S.Header>
+        <Delete />
+        <p>프로젝트 이름</p>
+      </S.Header>
+      <S.CharacterHeader>
+        <p>Character</p>
+        드롭다운 위치
+      </S.CharacterHeader>
+
+      <S.ButtonContainer>
+        <Button type="normal" color="gray" icon={<Add />} iconPosition="left">
+          Scenario
+        </Button>
+      </S.ButtonContainer>
+    </S.Container>
+  );
 }
