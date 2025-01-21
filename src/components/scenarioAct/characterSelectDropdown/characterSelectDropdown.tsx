0985f10f47b545957ef48ed8abad50a3fe80e10d
dohyung <154952683+dohyung001@users.noreply.github.com>
2025-01-21 16:10:38 +0900

    feat: 역할 선택 드롭다운 구현

--- src/components/scenarioAct/characterSelectDropdown/characterSelectDropdown.tsx
+++ src/components/scenarioAct/characterSelectDropdown/characterSelectDropdown.tsx
@@ -20,19 +20,17 @@ export default function CharacterSelectDropdown({ options, onSelect }: IDropdown
 
   // 옵션 선택 함수
   const handleOptionClick = (option: string) => {
-    setSelectedOption(option); // 선택된 옵션 업데이트
-    onSelect(option); // 부모 컴포넌트에 선택된 옵션 전달
-    setIsOpen(false); // 드롭다운 닫기
+    setSelectedOption(option);
+    onSelect(option);
+    setIsOpen(false);
   };
 
   return (
     <S.Container $isOpen={isOpen}>
-      {/* 선택된 옵션 및 드롭다운 토글 버튼 */}
-      <S.Content onClick={toggleDropdown}>
+      <S.Content onClick={toggleDropdown} $isOpen={isOpen}>
         {selectedOption} <ArrowDown />
       </S.Content>
 
-      {/* 드롭다운 옵션 리스트 */}
       {isOpen && (
         <S.Dropdown>
           {options.map((option, index) => (
