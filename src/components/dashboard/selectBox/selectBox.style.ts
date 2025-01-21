fde5626c8340be89b96200b285ee0910108b9587
hijjoy <hijjoy@naver.com>
2025-01-21 14:38:46 +0900

    feat: select 대시보드 연결

---
+++ src/components/dashboard/selectBox/selectBox.style.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const Container = styled.ul`
+  position: absolute;
+  top: 30px;
+  left: 0;
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 6.4px;
+  border: 0.8px solid #082659;
+  background: #16181c;
+
+  li {
+    ${({ theme }) => theme.text.medium_14};
+    width: 100%;
+    border-radius: 6.4px;
+    padding: 8px 16px;
+    white-space: nowrap;
+
+    &:hover {
+      cursor: pointer;
+      background: rgba(255, 255, 255, 0.1);
+    }
+  }
+`;
+
+export { Container };
