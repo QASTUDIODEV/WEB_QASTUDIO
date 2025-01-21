0e6be2df0d59626e6486f6b35ea089587224aae2
dohyung <154952683+dohyung001@users.noreply.github.com>
2025-01-21 17:03:56 +0900

    feat: 컨트롤러 디자인 수정

--- src/components/scenarioAct/scenarioDropdown/scenarioDropdown.tsx
+++ src/components/scenarioAct/scenarioDropdown/scenarioDropdown.tsx
@@ -1,23 +1,35 @@
+import { useState } from 'react';
+
 import ActionItem from '@/components/scenarioAct/actionItem/actionItem';
 import * as S from '@/components/scenarioAct/scenarioDropdown/scenarioDropdown.style';
 
 import ArrowDown from '@/assets/icons/arrow_down.svg?react';
-import Pause from '@/assets/icons/pause.svg?react';
+import ArrowUp from '@/assets/icons/arrow_up.svg?react';
 import Play from '@/assets/icons/play.svg?react';
 
 export default function ScenarioDropdown() {
+  const [isOpen, setIsOpen] = useState(false); // 열림/닫힘 상태 관리
+
+  const toggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <S.Container>
-      <S.ScenarioHeader>
-        <ArrowDown />
+      <S.ScenarioHeader onClick={toggleDropdown}>
+        {isOpen ? <ArrowUp /> : <ArrowDown />}
         <S.Title>Memo</S.Title>
         <Play />
       </S.ScenarioHeader>
 
-      <S.ActionList>
-        <S.ActionDescription># 사용자가 로그인 페이지로 이동하고 로그인한다.</S.ActionDescription>
-        <ActionItem />
-      </S.ActionList>
+      {isOpen && (
+        <S.ActionList>
+          <S.ActionDescription># 사용자가 로그인 페이지로 이동하고 로그인한다.</S.ActionDescription>
+          <ActionItem />
+          <ActionItem />
+          <ActionItem />
+        </S.ActionList>
+      )}
     </S.Container>
   );
 }
