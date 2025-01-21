26411f1f55b1b4f1ef85afa73deebc39143e3ec9
김연진 <kyj0719@gmail.com>
2025-01-21 14:13:41 +0900

    Revert "feat: 중간 백업"
    This reverts commit 71ffd6dd947d85162fae82c416391b8c201be709.

--- src/hooks/mypage/useGetUserInfo.ts
+++
@@ -1,9 +0,0 @@
-import { QUERY_KEYS } from '@/constants/querykeys/queryKeys';
-
-import { getUserInfo } from '@/apis/userController/userController';
-
-import { useCoreQuery } from '../common/customQuery';
-
-export default function useGetUserInfo() {
-  return useCoreQuery(QUERY_KEYS.GET_USER_INFO, () => getUserInfo());
-}
