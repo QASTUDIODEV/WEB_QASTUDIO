bdb57f5a3d1b4acaf893eaad1ecd6764c7bcc610
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-01-23 23:42:49 +0900

    feat: 액션 세부사항 추가

--- src/components/scenarioAct/actionItem/actionItem.style.ts
+++ src/components/scenarioAct/actionItem/actionItem.style.ts
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 import { ACTION_STATE } from '@/enums/enums';
 
-export const Container = styled.div<{ state: ACTION_STATE; isLastAction: boolean }>`
+export const Container = styled.div`
+  position: relative;
+`;
+export const Header = styled.div<{ state: ACTION_STATE; $isLastAction: boolean }>`
   display: flex;
   padding: 10px 30px;
   align-items: center;
-  position: relative;
   justify-content: space-between;
-
   &::after {
     content: '';
     position: absolute;
@@ -16,8 +17,8 @@ export const Container = styled.div<{ state: ACTION_STATE; isLastAction: boolean
     left: 7.5px;
     right: 7.5px;
     height: 2px;
-    background-color: ${({ theme, state, isLastAction }) =>
-      isLastAction
+    background-color: ${({ theme, state, $isLastAction }) =>
+      $isLastAction
         ? state === ACTION_STATE.SUCCESS
           ? theme.colors.point.point_1
           : state === ACTION_STATE.ERROR
@@ -41,7 +42,7 @@ export const ActionName = styled.div`
 
 export const UnderIcon = styled.div`
   position: absolute;
-  bottom: -20px;
+  bottom: -19px;
   left: 0;
 `;
 
@@ -49,4 +50,28 @@ export const ActionType = styled.div`
   padding: 3px 10px;
   border-radius: 4px;
   border: 1px solid ${({ theme }) => theme.colors.primary.pri_50};
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const DescriptionItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 9px 64px;
+`;
+export const DescriptionRow = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+export const Input = styled.input`
+  padding: 4px 10px;
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.primary.pri_50};
+  color: ${({ theme }) => theme.colors.primary.pri_50};
+  background-color: inherit;
+  outline: none;
+  width: 200px;
 `;
