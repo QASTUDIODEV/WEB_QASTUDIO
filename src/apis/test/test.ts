7f5f91de24641b32932fea44cf37ab318797a035
hijjoy <hijjoy@naver.com>
2025-01-24 00:41:19 +0900

    fix: api post -> get으로 수정

--- src/apis/test/test.ts
+++ src/apis/test/test.ts
@@ -11,7 +11,7 @@ import { axiosInstance } from '../axiosInstance';
 
 // 테스트 리스트 조회 API
 export const getTestList = async ({ projectId, date, state, pageName, page }: TGetTestListRequest): Promise<TGetTestListResponse> => {
-  const { data } = await axiosInstance.post(`/api/v0/projects/${projectId}/tests`, {
+  const { data } = await axiosInstance.get(`/api/v0/projects/${projectId}/tests`, {
     params: { page, pageName, date, state },
   });
 
@@ -20,14 +20,14 @@ export const getTestList = async ({ projectId, date, state, pageName, page }: TG
 
 // 테스트 통계 조회 API
 export const getStatistics = async ({ projectId }: TGetStatisticsRequest): Promise<TGetStatisticsResponse> => {
-  const { data } = await axiosInstance.post(`/api/v0/projects/${projectId}/tests/statistics`);
+  const { data } = await axiosInstance.get(`/api/v0/projects/${projectId}/tests/statistics`);
 
   return data;
 };
 
 // 테스트 검색 API
 export const searchTestList = async ({ projectId, testName, pageName, page, date, state }: TSearchTestListRequest): Promise<TSearchTestListResponse> => {
-  const { data } = await axiosInstance.post(`/api/v0/projects/${projectId}/tests/search`, {
+  const { data } = await axiosInstance.get(`/api/v0/projects/${projectId}/tests/search`, {
     params: { testName, page, pageName, date, state },
   });
 
