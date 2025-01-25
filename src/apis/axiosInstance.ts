32c42801dcf8bd1f6a1fbf4b7c2250dc4972cb1f
김연진 <kyj0719@gmail.com>
2025-01-25 17:44:34 +0900

    feat: 리프레시 함수 주석처리, 임의로 값 지정(임시)

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,11 +1,7 @@
-import type { AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
-import type { TToken } from '@/types/auth/auth';
-
-import { refresh } from './auth/auth';
-
-import { logout, refreshToken } from '@/slices/authSlice';
+// import { refresh } from './auth/auth';
+import { logout } from '@/slices/authSlice';
 import store from '@/store/store';
 
 export const axiosInstance = axios.create({
@@ -32,72 +28,46 @@ axiosInstance.interceptors.request.use(
 
 let isRedirecting = false;
 axiosInstance.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    console.log('response', response);
+    return response;
+  },
   async (error) => {
-    if (error.response?.status !== 401) {
-      return Promise.reject(error);
-    }
-
-    const refreshTokenData = localStorage.getItem('refreshToken');
-    if (!refreshTokenData) {
-      handleUnauthenticated();
-      return;
-    }
-
-    try {
-      const response = await refresh();
-
-      if (response.status === 200) {
-        handleTokenRefreshSuccess(response.data, error.config);
-        return axiosInstance.request(error.config);
-      } else if (response.status === 401) {
-        handleTokenRefreshFailure();
+    if (error.response?.status === 401 && isRedirecting === false) {
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (refreshToken) {
+        try {
+          // const refreshResponse = await refresh();
+          const refreshResponse = { code: 401 }; // 현재 리프레시 api가 없어 임시로 지정해두었습니다 현재 무조건 401을 반환하여 리다이렉트됩니다.
+          if (refreshResponse.code === 200) {
+            console.log('refreshToken이 재발급 되었습니다');
+          } else {
+            if (isRedirecting === false) {
+              console.log('refreshToken이 만료되었습니다. 로그인 페이지로 이동합니다.');
+              isRedirecting = true;
+              store.dispatch(logout());
+              window.location.href = '/';
+            }
+          }
+        } catch {
+          if (isRedirecting === false) {
+            store.dispatch(logout());
+            isRedirecting = true;
+            window.location.href = '/';
+          }
+        }
       } else {
-        handleUnknownError(response.status);
+        console.log('refreshToken이 없습니다. 로그인 페이지로 이동합니다.');
+        if (isRedirecting === false) {
+          isRedirecting = true;
+          localStorage.removeItem('refreshToken');
+          localStorage.removeItem('accessToken');
+          store.dispatch(logout());
+          window.location.href = '/';
+        }
       }
-    } catch (refreshError) {
-      console.error('토큰 재발급 중 오류 발생:', refreshError);
-      handleUnauthenticated();
     }
 
     return Promise.reject(error);
   },
 );
-
-function handleUnauthenticated() {
-  console.log('refreshToken이 없거나 만료되었습니다. 로그인 페이지로 이동합니다.');
-  clearAuthData();
-  redirectToLogin();
-}
-
-function handleTokenRefreshSuccess(tokens: TToken, originalRequestConfig: AxiosRequestConfig) {
-  console.log('토큰이 재발급되었습니다.');
-  localStorage.setItem('accessToken', tokens.accessToken);
-  localStorage.setItem('refreshToken', tokens.refreshToken);
-  originalRequestConfig.headers = originalRequestConfig.headers || {};
-  originalRequestConfig.headers.Authorization = `Bearer ${tokens.accessToken}`;
-  store.dispatch(refreshToken({ accessToken: tokens.accessToken }));
-}
-
-function handleTokenRefreshFailure() {
-  console.log('refreshToken이 만료되었습니다. 다시 로그인해주세요.');
-  clearAuthData();
-  redirectToLogin();
-}
-
-function handleUnknownError(status: number) {
-  console.log('알 수 없는 오류가 발생했습니다.', status);
-  clearAuthData();
-  redirectToLogin();
-}
-
-function clearAuthData() {
-  store.dispatch(logout());
-}
-
-function redirectToLogin() {
-  if (!isRedirecting) {
-    isRedirecting = true;
-    window.location.href = '/';
-  }
-}
