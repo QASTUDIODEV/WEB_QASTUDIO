c4124174d725c7029dd7540c19a6ca0a308050e3
hijjoy <hijjoy@naver.com>
2025-01-27 10:17:31 +0900

    fix: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), svgr({ include: '**/*.svg?react' })],
   resolve: {
@@ -12,9 +12,12 @@ export default defineConfig({
     },
   },
   server: {
-    https: {
-      key: fs.readFileSync('.cert/localhost-key.pem'),
-      cert: fs.readFileSync('.cert/localhost.pem'),
-    },
+    https:
+      process.env.NODE_ENV === 'development'
+        ? {
+            key: fs.readFileSync('.cert/localhost-key.pem'),
+            cert: fs.readFileSync('.cert/localhost.pem'),
+          }
+        : undefined,
   },
 });
