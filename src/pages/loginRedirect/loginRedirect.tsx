9f55f05efae651ab9a07641608950cfe7e68480c
김연진 <kyj0719@gmail.com>
2025-01-27 14:21:02 +0900

    feat: 소셜 로그인 구현

--- src/pages/loginRedirect/loginRedirect.tsx
+++ src/pages/loginRedirect/loginRedirect.tsx
@@ -2,39 +2,30 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { getCookie } from '@/utils/cookies';
+import useProjectList from '@/hooks/sidebar/sidebar';
 
 import { isSignup, login } from '@/slices/authSlice';
-// 일단 작동을 안하니까 무시해주세여......
+
 function LoginRedirect() {
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch();
+
+  const { useGetSidbarUserInfo } = useProjectList();
+  const { data: userInfo, isError } = useGetSidbarUserInfo;
+
   useEffect(() => {
     if (sessionStorage.getItem('loginHandled') == null) {
-      const existing_user = getCookie('existing_user') ?? '';
-      const accessToken = getCookie('accessToken') ?? '';
-      const refreshToken = getCookie('refreshToken') ?? '';
-      if (accessToken === '' && refreshToken === '') {
-        alert('로그인 또는 회원가입에 실패하였습니다.');
+      if (isError) {
         navigate('/');
-        return;
       }
-      if (existing_user === 'false') {
-        sessionStorage.setItem('loginHandled', 'true');
-        dispatch(login({ accessToken: '', refreshToken: '' }));
-        dispatch(isSignup({ isSignup: true }));
+      if (userInfo?.result.nickname === '') {
         navigate('/signup/userSetting');
-        return;
-      } else if (existing_user === 'true') {
-        sessionStorage.setItem('loginHandled', 'true');
-        dispatch(login({ accessToken: '', refreshToken: '' }));
-        navigate('/project');
-        return;
+        dispatch(isSignup({ isSignup: true }));
+        dispatch(login());
       } else {
-        alert('로그인 또는 회원가입에 실패하였습니다.');
-        navigate('/');
-        return;
+        dispatch(login());
+        navigate('/project');
       }
     }
   }, [navigate, location]);
