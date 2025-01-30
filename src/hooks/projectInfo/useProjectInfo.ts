3989f0215a5a9d18922c6dff9f313970d7beec01
lwittyl <mercury0502@dgu.ac.kr>
2025-01-30 16:48:44 +0900

    feat: 팀원 조회 api 연결

--- src/hooks/projectInfo/useProjectInfo.ts
+++ src/hooks/projectInfo/useProjectInfo.ts
@@ -1,12 +1,13 @@
 import type { TProjectInfo } from '@/types/projectInfo/projectInfo';
 import { QUERY_KEYS } from '@/constants/querykeys/queryKeys';
 
-import { editIntroduce, getProjectInfo } from '@/apis/projectInfo/projectInfo';
+import { editIntroduce, getProjectInfo, getProjectMember } from '@/apis/projectInfo/projectInfo';
 
 import { useCoreMutation, useCoreQuery } from '../common/customQuery';
 
 export function useProjectInfo({ projectId }: TProjectInfo) {
   const useProjectExtractInfo = useCoreQuery(QUERY_KEYS.PROJECT_INFO({ projectId }), () => getProjectInfo({ projectId }));
   const useEditIntroduce = useCoreMutation(editIntroduce, {});
-  return { useProjectExtractInfo, useEditIntroduce };
+  const useGetProjectMember = useCoreQuery(QUERY_KEYS.PROJECT_MEMBER({ projectId }), () => getProjectMember({ projectId }));
+  return { useProjectExtractInfo, useEditIntroduce, useGetProjectMember };
 }
