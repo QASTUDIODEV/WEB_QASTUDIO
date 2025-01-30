6d7e4b1ba4c926a5cfaf45c3f9d58e4d87a243d7
lwittyl <mercury0502@dgu.ac.kr>
2025-01-30 17:47:58 +0900

    chore: console 삭제

--- src/components/common/sidebar/projectModal/projectModal.tsx
+++ src/components/common/sidebar/projectModal/projectModal.tsx
@@ -87,7 +87,6 @@ export default function ProjectModal({ projectLength, onClose }: TProjectModalPr
   }, [data, debouncedEmail]);
 
   const handleAddEmail = () => {
-    console.log(emails);
     if (!debouncedEmail || emails.includes(debouncedEmail)) {
       return; // 이메일이 비어 있거나 이미 추가된 경우
     }
