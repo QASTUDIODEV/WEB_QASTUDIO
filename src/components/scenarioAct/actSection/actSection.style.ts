d37b4472af63888e354825ef972dffd122826942
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-02-03 00:08:59 +0900

    feat: 스크롤 수정

--- src/components/scenarioAct/actSection/actSection.style.ts
+++ src/components/scenarioAct/actSection/actSection.style.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background: ${({ theme }) => theme.colors.primary.pri_900};
-  background-color: red;
   height: 100%;
+  overflow: hidden;
 `;
