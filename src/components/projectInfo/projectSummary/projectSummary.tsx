20db194af1a88e4f948792e554898c8113c83d3e
lwittyl <mercury0502@dgu.ac.kr>
2025-02-09 02:55:22 +0900

    fix: summary 분리

---
+++ src/components/projectInfo/projectSummary/projectSummary.tsx
@@ -0,0 +1,135 @@
+import { useState } from 'react';
+
+import { useDispatch } from '@/hooks/common/useCustomRedux.ts';
+import { useProjectInfo } from '@/hooks/projectInfo/useProjectInfo';
+
+import Button from '@/components/common/button/button';
+import { MODAL_TYPES } from '@/components/common/modalProvider/modalProvider.tsx';
+
+import Plus from '@/assets/icons/add.svg?react';
+import Branch from '@/assets/icons/branch_white.svg?react';
+import File from '@/assets/icons/files.svg?react';
+import Rights from '@/assets/icons/shield.svg?react';
+import * as S from '@/pages/projectInfo/projectInfo.style';
+import ProjectStructure from '@/pages/projectInfo/projectStructure';
+import { openModal } from '@/slices/modalSlice.ts';
+
+type TProjectInfoProps = {
+  result:
+    | {
+        projectId: number;
+        projectImage: string;
+        projectName: string;
+        projectUrl: string;
+        introduction: string;
+        viewType: string;
+        developmentSkill: string;
+        assistantId: string;
+      }
+    | undefined;
+};
+
+export default function ProjectInfoPage({ result }: TProjectInfoProps) {
+  const modalDispatch = useDispatch();
+  const [isStructureVisible, setIsStructureVisible] = useState(true);
+  const [selectedPage, setSelectedPage] = useState(0);
+  const { useGetPageSummary, useGetCharacter } = useProjectInfo({ projectId: Number(result?.projectId) });
+  const { data: summaryList } = useGetPageSummary;
+  const { data: characterList } = useGetCharacter;
+  const summary = summaryList?.result.pageSummaryList;
+  const character = characterList?.result.detailCharacters;
+  const accessed = summary?.map((a) => a.hasAccess);
+  const notAccessed = summary?.map((a) => a.deniedAccess);
+  return (
+    <>
+      {isStructureVisible ? (
+        <>
+          <S.Title>Project structure</S.Title>
+          <S.TextBold>Summary</S.TextBold>
+          <S.TextLight>{result?.introduction}</S.TextLight>
+          <S.Wrapper top="16px" right="24px">
+            <Plus
+              onClick={() =>
+                modalDispatch(openModal({ modalType: MODAL_TYPES.CreatePageModal, modalProps: { projectId: Number(result?.projectId), character: character } }))
+              }
+            />
+          </S.Wrapper>
+          <S.InnerBox>
+            <div style={{ flex: 1 }}>
+              <S.Table>
+                <thead>
+                  <tr>
+                    <S.TH>
+                      <File /> Page
+                    </S.TH>
+                    <S.TH>
+                      <Branch /> Path
+                    </S.TH>
+                    <S.TH>
+                      <Rights /> Access Rights
+                    </S.TH>
+                  </tr>
+                </thead>
+                <tbody>
+                  {summary &&
+                    summary.map((a, index) => (
+                      <S.TR
+                        key={a.pageId}
+                        onClick={() => {
+                          setIsStructureVisible(false);
+                          setSelectedPage(index);
+                        }}
+                      >
+                        <S.TD>{a.pageName}</S.TD>
+                        <S.TD>{a.path}</S.TD>
+                        <S.TD>
+                          <S.AccessRights>
+                            <S.Button>
+                              <Button type="small_round" color="green">
+                                접근 가능
+                              </Button>
+                            </S.Button>
+                            {accessed &&
+                              accessed[index].map((role, i) => (
+                                <S.Button key={i}>
+                                  <Button type="small_round" color="white_round">
+                                    {role}
+                                  </Button>
+                                </S.Button>
+                              ))}
+
+                            <S.Button>
+                              <Button type="small_round" color="red">
+                                접근 불가능
+                              </Button>
+                            </S.Button>
+                            {notAccessed &&
+                              notAccessed[index].map((role, i) => (
+                                <S.Button key={i}>
+                                  <Button type="small_round" color="white_round">
+                                    {role}
+                                  </Button>
+                                </S.Button>
+                              ))}
+                          </S.AccessRights>
+                        </S.TD>
+                      </S.TR>
+                    ))}
+                </tbody>
+              </S.Table>
+            </div>
+          </S.InnerBox>
+        </>
+      ) : (
+        <ProjectStructure
+          selectedPage={selectedPage}
+          setSelectedPage={(page) => setSelectedPage(page)}
+          onBackToSummary={() => setIsStructureVisible(true)}
+          pageData={summary}
+          projectId={Number(result?.projectId)}
+          character={character}
+        />
+      )}
+    </>
+  );
+}
--- src/pages/projectInfo/projectInfo.tsx
+++ src/pages/projectInfo/projectInfo.tsx
@@ -11,35 +11,26 @@ import { MODAL_TYPES } from '@/components/common/modalProvider/modalProvider.tsx
 import Profile from '@/components/common/profile/profile';
 import ProjectInput from '@/components/projectInfo/projectInput/projectInput';
 import ProjectIntro from '@/components/projectInfo/projectIntroduction/projectIntro';
+import ProjectSummary from '@/components/projectInfo/projectSummary/projectSummary';
 import ToolTip from '@/components/projectInfo/toolTip/toolTip';
 
 import Plus from '@/assets/icons/add.svg?react';
 import ArrowRight from '@/assets/icons/arrow_right.svg?react';
 import Book from '@/assets/icons/book.svg?react';
-import Branch from '@/assets/icons/branch_white.svg?react';
 import Crown from '@/assets/icons/crown.svg?react';
-import File from '@/assets/icons/files.svg?react';
 import Page from '@/assets/icons/page.svg?react';
-import Rights from '@/assets/icons/shield.svg?react';
 import * as S from '@/pages/projectInfo/projectInfo.style';
-import ProjectStructure from '@/pages/projectInfo/projectStructure';
 import { openModal } from '@/slices/modalSlice.ts';
 
 export default function ProjectInfoPage({ projectInfo }: { projectInfo?: TGetProjectInfo }) {
   const result = projectInfo?.result;
-  const [isStructureVisible, setIsStructureVisible] = useState(true);
-  const [selectedPage, setSelectedPage] = useState(0);
-  const { useGetProjectMember, useGetPageSummary, useGetCharacter } = useProjectInfo({ projectId: Number(result?.projectId) });
+  const { useGetProjectMember, useGetCharacter } = useProjectInfo({ projectId: Number(result?.projectId) });
   const { data: members } = useGetProjectMember;
-  const { data: summaryList } = useGetPageSummary;
   const { data: characterList } = useGetCharacter;
-  const summary = summaryList?.result.pageSummaryList;
 
   const modalDispatch = useDispatch();
   const character = characterList?.result.detailCharacters;
   const member = members?.result.members;
-  const accessed = summary?.map((a) => a.hasAccess);
-  const notAccessed = summary?.map((a) => a.deniedAccess);
   const tooltipRef = useRef<HTMLDivElement | null>(null);
   const [activeTooltip, setActiveTooltip] = useState<number | null>(null);
   const handleMouseEnter = (characterId: number) => {
@@ -61,102 +52,8 @@ export default function ProjectInfoPage({ projectInfo }: { projectInfo?: TGetPro
       </S.Box>
       <S.SemiBox>
         <S.Left>
-          <S.Box
-            height="59%"
-            style={{
-              cursor: 'pointer',
-            }}
-          >
-            {isStructureVisible ? (
-              <>
-                <S.Title>Project structure</S.Title>
-                <S.TextBold>Summary</S.TextBold>
-                <S.TextLight>{result?.introduction}</S.TextLight>
-                <S.Wrapper top="16px" right="24px">
-                  <Plus
-                    onClick={() =>
-                      modalDispatch(
-                        openModal({ modalType: MODAL_TYPES.CreatePageModal, modalProps: { projectId: Number(result?.projectId), character: character } }),
-                      )
-                    }
-                  />
-                </S.Wrapper>
-                <S.InnerBox>
-                  <div style={{ flex: 1 }}>
-                    <S.Table>
-                      <thead>
-                        <tr>
-                          <S.TH>
-                            <File /> Page
-                          </S.TH>
-                          <S.TH>
-                            <Branch /> Path
-                          </S.TH>
-                          <S.TH>
-                            <Rights /> Access Rights
-                          </S.TH>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {summary &&
-                          summary.map((a, index) => (
-                            <S.TR
-                              key={a.pageId}
-                              onClick={() => {
-                                setIsStructureVisible(false);
-                                setSelectedPage(index);
-                              }}
-                            >
-                              <S.TD>{a.pageName}</S.TD>
-                              <S.TD>{a.path}</S.TD>
-                              <S.TD>
-                                <S.AccessRights>
-                                  <S.Button>
-                                    <Button type="small_round" color="green">
-                                      접근 가능
-                                    </Button>
-                                  </S.Button>
-                                  {accessed &&
-                                    accessed[index].map((role, i) => (
-                                      <S.Button key={i}>
-                                        <Button type="small_round" color="white_round">
-                                          {role}
-                                        </Button>
-                                      </S.Button>
-                                    ))}
-
-                                  <S.Button>
-                                    <Button type="small_round" color="red">
-                                      접근 불가능
-                                    </Button>
-                                  </S.Button>
-                                  {notAccessed &&
-                                    notAccessed[index].map((role, i) => (
-                                      <S.Button key={i}>
-                                        <Button type="small_round" color="white_round">
-                                          {role}
-                                        </Button>
-                                      </S.Button>
-                                    ))}
-                                </S.AccessRights>
-                              </S.TD>
-                            </S.TR>
-                          ))}
-                      </tbody>
-                    </S.Table>
-                  </div>
-                </S.InnerBox>
-              </>
-            ) : (
-              <ProjectStructure
-                selectedPage={selectedPage}
-                setSelectedPage={(page) => setSelectedPage(page)}
-                onBackToSummary={() => setIsStructureVisible(true)}
-                pageData={summary}
-                projectId={Number(result?.projectId)}
-                character={character}
-              />
-            )}
+          <S.Box height="59%">
+            <ProjectSummary result={result} />
           </S.Box>
           <S.Box height="35%">
             <S.Title>Character</S.Title>
