7ec18fdc874873337cc914d7f909d78c523e0d4a
hyewon <89793335+hijjoy@users.noreply.github.com>
2025-02-12 21:05:11 +0900

    ✨ [Feature] 표 select ALL filter 추가
    * feat: 표 params로 정의 #239
    
    * feat: 표 선택bar에 all 추가 #239
    
    * fix: 필터링 오류 수정 #239
    - setFilters props로 전달

--- src/components/dashboard/selectBox/selectBox.tsx
+++ src/components/dashboard/selectBox/selectBox.tsx
@@ -1,18 +1,25 @@
 import type { ReactNode } from 'react';
 
+import type { TEST_STATE } from '@/enums/enums';
+
+import { getSelectName } from '@/utils/getSelectName';
+
+import type { TFilter } from '@/hooks/dashborad/useTableFilter';
+
 import * as S from './selectBox.style';
 
 type TSelectBoxProps<T extends ReactNode> = {
   selectList: T[];
-  onSelect: (value: T) => void;
+  filterKey: keyof TFilter;
+  setFilters: (filter: TFilter) => void;
 };
 
-export default function SelectBox<T extends ReactNode>({ selectList, onSelect }: TSelectBoxProps<T>) {
+export default function SelectBox<T extends ReactNode>({ selectList, setFilters, filterKey }: TSelectBoxProps<T>) {
   return (
     <S.Container>
       {selectList.map((e, idx) => (
-        <li key={idx} onClick={() => onSelect(e)}>
-          {e}
+        <li key={idx} onClick={() => setFilters({ [filterKey]: e })}>
+          {filterKey === 'state' ? getSelectName(e as TEST_STATE) : e}
         </li>
       ))}
     </S.Container>
