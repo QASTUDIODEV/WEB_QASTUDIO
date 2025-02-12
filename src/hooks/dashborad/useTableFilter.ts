3fbf98bc6fb31aae58082b4da8d20cf7c173cb41
hijjoy <hijjoy@naver.com>
2025-02-12 20:47:20 +0900

    feat: 표 params로 정의

---
+++ src/hooks/dashborad/useTableFilter.ts
@@ -0,0 +1,39 @@
+import { useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import type { TEST_STATE } from '@/enums/enums';
+
+export type TFilter = {
+  testName?: string;
+  pageName?: string;
+  state?: TEST_STATE;
+  date?: string;
+};
+
+export default function useTableFilter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const state = searchParams.get('state') ? (searchParams.get('state') as TEST_STATE) : undefined;
+  const pageName = searchParams.get('pageName') ? (searchParams.get('pageName') as string) : undefined;
+  const testName = searchParams.get('testName') ? (searchParams.get('testName') as string) : undefined;
+  const date = searchParams.get('date') ? (searchParams.get('date') as string) : undefined;
+
+  const setFilters = useCallback((filters: TFilter) => {
+    setSearchParams((params) => {
+      if (filters.state !== undefined) params.set('state', filters.state);
+      if (filters.pageName !== undefined) params.set('pageName', filters.pageName);
+      if (filters.testName !== undefined) params.set('testName', filters.testName);
+      if (filters.date !== undefined) params.set('date', filters.date);
+
+      return params;
+    });
+  }, []);
+
+  return {
+    date,
+    state,
+    pageName,
+    testName,
+    setFilters,
+  };
+}
