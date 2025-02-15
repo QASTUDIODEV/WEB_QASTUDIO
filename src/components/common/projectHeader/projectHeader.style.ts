45017ccacfedb7e0adaa42cbf8cf42ea6ef93ef8
김연진 <kyj0719@gmail.com>
2025-02-16 00:03:04 +0900

    feat: 디자인 변경 수정

--- src/components/common/projectHeader/projectHeader.style.ts
+++ src/components/common/projectHeader/projectHeader.style.ts
@@ -4,7 +4,7 @@ export const Header = styled.div`
   display: flex;
   width: 100%;
   padding: 50px 60px 0px 60px;
-  justify-content: space-between;
+  gap: 10px;
 `;
 export const ButtonWrapper = styled.div`
   @media (max-width: 610px) {
