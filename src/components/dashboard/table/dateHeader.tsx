e1928c27e4b5591a2d8ab641c6e74afaa36b5b7a
hijjoy <hijjoy@naver.com>
2025-02-15 12:15:56 +0900

    fix: 캘린더 오류 수정 - redux로 캘린더 닫고 열리는 상태관리 - 날짜 클릭시에만 닫히고, 월별 변경 클릭에는 닫히지 않도록 수정

--- src/components/dashboard/table/dateHeader.tsx
+++ src/components/dashboard/table/dateHeader.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react';
 import type { Column } from '@tanstack/react-table';
 
 import type { TTestListDTO } from '@/types/test/test';
+
+import { useDispatch, useSelector } from '@/hooks/common/useCustomRedux';
 
 import * as S from '@/components/dashboard/table/table.style';
 
 import Calendar from '../calendar/calendar';
 
 import { DownArrow, UpArrow } from '@/assets/icons';
+import { toggleCalendar } from '@/slices/calendarSlice.ts';
 
 interface IProps {
   column: Column<TTestListDTO, string>;
 }
 
 export default function DateHeader({ column }: IProps) {
-  const [isClicked, setIsClicked] = useState(false);
+  const dispatch = useDispatch();
+  const isClicked = useSelector((state) => state.calendar.isCalendarOpen);
 
   return (
     <S.HeaderWrapper>
-      <S.ButtonHeader onClick={() => setIsClicked((prev) => !prev)}>
+      <S.ButtonHeader onClick={() => dispatch(toggleCalendar())}>
         <p>Date</p>
         {column.getIsSorted() ? <UpArrow /> : <DownArrow />}
       </S.ButtonHeader>
