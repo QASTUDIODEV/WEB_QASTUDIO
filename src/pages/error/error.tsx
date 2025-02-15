4bb473a8f0be5ce524e866b21ae155fb26476da6
hijjoy <hijjoy@naver.com>
2025-02-15 23:48:38 +0900

    feat: not-found-error page button 추가 - "/"로 경로 이동 버튼 추가 - 버튼 뒤로가기 방지

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,13 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 import * as S from '@/layouts/auth/auth.style.ts';
 
-import { TextWrapper } from '@/pages/error/error.style';
+import { BackIcon } from '@/assets/icons';
+import { Button, TextWrapper } from '@/pages/error/error.style';
 
 export default function ErrorPage() {
+  const navigate = useNavigate();
+
   return (
     <S.Container>
       <TextWrapper>
-        <h1>Not found page</h1>
-        <p>The page could not be found, please try again in a moment.</p>
+        <div>
+          <h1>Not found page</h1>
+          <p>The page could not be found, please try again in a moment.</p>
+        </div>
+        <Button onClick={() => navigate('/', { replace: true })}>
+          <BackIcon />
+          <p>Go back to the main page</p>
+        </Button>
       </TextWrapper>
       <S.Circle1 />
       <S.Circle2 />
