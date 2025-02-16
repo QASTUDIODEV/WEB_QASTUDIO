3112a052270a30deb0a8615a07c0ae83303a25c1
김연진 <kyj0719@gmail.com>
2025-02-16 17:34:29 +0900

    fix: 에러처리

--- src/components/common/profile/profile.tsx
+++ src/components/common/profile/profile.tsx
@@ -1,5 +1,8 @@
 import * as S from '@/components/common/profile/profile.style';
 
+import DefaultProfileImg from '@/assets/icons/defaultProfile.svg';
+import DefaultProjectImg from '@/assets/icons/DefaultProject.svg';
+
 import DefaultProfile from '@/assets/icons/defaultProfile.svg?react';
 import DefaultProject from '@/assets/icons/DefaultProject.svg?react';
 
@@ -11,7 +14,13 @@ export default function Profile({ profileImg, isProject = false }: TProfileProps
   return (
     <>
       {profileImg ? (
-        <S.Img src={profileImg} alt="Profile Image" />
+        <S.Img
+          src={profileImg}
+          alt="Profile Image"
+          onError={(e) => {
+            isProject ? (e.currentTarget.src = DefaultProjectImg) : (e.currentTarget.src = DefaultProfileImg);
+          }}
+        />
       ) : isProject ? (
         <DefaultProject style={{ width: '100%', height: '100%' }} />
       ) : (
