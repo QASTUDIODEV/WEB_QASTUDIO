ff677d4bbbbfecde1573a5de9056a292dab8c352
dohyung001 <154952683+dohyung001@users.noreply.github.com>
2025-02-20 00:46:03 +0900

    fix: 빌드 에러 해결

--- src/pages/scenarioAct/scenarioAct.tsx
+++ src/pages/scenarioAct/scenarioAct.tsx
@@ -29,12 +29,12 @@ export default function ScenarioActPage() {
 
   //프로젝트 정보
   const { useGetProjectInfo, useGetCharacterList } = useProjectInfo(projectId);
-  const { data: projectInfo, isLoading: projectInfoLoading, error: projectInfoError } = useGetProjectInfo;
-  const { isLoading: characterListLoading, error: characterListError } = useGetCharacterList;
+  const { data: projectInfo, isLoading: projectInfoLoading } = useGetProjectInfo;
+  const { isLoading: characterListLoading } = useGetCharacterList;
 
   // 시나리오 리스트 불러오기
   const { useGetScenarioList } = useScenarioList(selectedCharacterId);
-  const { isLoading: scenarioListLoading, error: scenarioListError } = useGetScenarioList;
+  const { isLoading: scenarioListLoading } = useGetScenarioList;
 
   // 시나리오 리스트는 refetch할 때 선언되서 useEffect 분리
   useEffect(() => {
@@ -61,8 +61,6 @@ export default function ScenarioActPage() {
   }, [fetchPageSource, dispatch, projectInfo?.result?.projectUrl]);
 
   const isLoading = projectInfoLoading || characterListLoading || scenarioListLoading || isPending;
-  const errors = [projectInfoError, characterListError, scenarioListError].filter(Boolean);
-  //const errorCode = errors.some((err) => (err as TAxiosResponseError)?.response?.data?.code === 'PROJECT403');
 
   return (
     <S.Container>
