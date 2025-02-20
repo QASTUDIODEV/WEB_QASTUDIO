43001b765625ab77b32ecd3d959e720059f44599
김연진 <kyj0719@gmail.com>
2025-02-20 16:17:12 +0900

    feat: 로그아웃 하시겠습니까 모달에서 토큰이 없는 경우 로그아웃도 안되고 서비스 이용도 안되는 에러 수정

--- src/components/common/sidebar/logtoutModal/logoutModal.tsx
+++ src/components/common/sidebar/logtoutModal/logoutModal.tsx
@@ -22,6 +22,9 @@ export default function LogoutModal({ onClose }: TLogoutModalProps) {
           navigate('/', { replace: true });
           onClose();
         },
+        onError: () => {
+          navigate('/', { replace: true });
+        },
       },
     );
   };
